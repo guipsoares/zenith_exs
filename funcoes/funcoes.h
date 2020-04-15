4d2e904ffa9e8147f0ea0a0ba691672d68cfe61b
guipsoares <gui.pedri.soares@hotmail.com>
2020-04-15 19:38:07 -0300

    biblioteca

--- funcoes/funcoes.h
+++ funcoes/funcoes.h
@@ -3,8 +3,15 @@
 #ifndef _FUNCOES
 #define _FUNCOES
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int soma(int i, int j, int k);
 double media(int i, int j, int k);
 double desvio(int i, int j, int k);
 
+#endif
+#ifdef __cplusplus
+}
 #endif
